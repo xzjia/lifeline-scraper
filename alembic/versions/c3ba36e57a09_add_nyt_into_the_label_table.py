23f9af8fc929f85ea0b48cab432fb9365bcf7ca3
Xiaozhou Jia <jiaxiaozhou@gmail.com>
2018-06-06 10:41:28 +0900

    Add migration of adding NYT into label table

---
+++ alembic/versions/c3ba36e57a09_add_nyt_into_the_label_table.py
@@ -0,0 +1,24 @@
+"""Add nyt into the label table
+
+Revision ID: c3ba36e57a09
+Revises: 0ab75264a276
+Create Date: 2018-06-06 10:24:50.006292
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c3ba36e57a09'
+down_revision = '0ab75264a276'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("INSERT INTO label (name) VALUES('New York Times');")
+
+
+def downgrade():
+    pass
