fb7f5ac521345bf9a094617fc112dc9b467f374f
Xiaozhou Jia <jiaxiaozhou@gmail.com>
2018-06-07 11:58:49 +0900

    Remove hard coded label id from migration

--- alembic/versions/9ae401c6c30c_add_nyt_articles_for_june_2018.py
+++ alembic/versions/9ae401c6c30c_add_nyt_articles_for_june_2018.py
@@ -11,7 +11,6 @@ from alembic import op
 import sqlalchemy as sa
 
 
-
 # revision identifiers, used by Alembic.
 revision = '9ae401c6c30c'
 down_revision = 'c3ba36e57a09'
@@ -19,26 +18,41 @@ branch_labels = None
 depends_on = None
 
 
+def get_db_conn():
+    engine = sa.create_engine(
+        os.environ['DATABASE_URL'], echo=True)
+    return engine.connect()
+
+
+def get_label_id_from_name(name):
+    label_table = sa.table('label', sa.column(
+        'id', sa.Integer), sa.column('name', sa.Text))
+    s = sa.sql.select([label_table.c.id, label_table.c.name]
+                      ).where(label_table.c.name == name)
+    conn = get_db_conn()
+    result = conn.execute(s)
+    return result.fetchone()['id']
+
+
 def upgrade():
-    label_table = sa.table('label')
-    event_table = sa.table('event',    
-        sa.column('timestamp', sa.DateTime),
-        sa.column('title', sa.Text),
-        sa.column('text', sa.Text),
-        sa.column('link', sa.Text),
-        sa.column('label_id', sa.Integer)
-    )
-    label_id = op.execute("select id from label where name='New York Times'")
+    event_table = sa.table('event',
+                           sa.column('timestamp', sa.DateTime),
+                           sa.column('title', sa.Text),
+                           sa.column('text', sa.Text),
+                           sa.column('link', sa.Text),
+                           sa.column('label_id', sa.Integer)
+                           )
+    label_id = get_label_id_from_name('New York Times')
     data = []
     for filename in os.listdir('nyt/archive'):
         with open('nyt/archive/'+filename) as infile:
             data.extend(json.load(infile))
-    mapped_data = list(map(lambda x: {'timestamp': x['pub_date'], 'title': x['headline']['print_headline'], 'text': x['snippet'], 'link': x['web_url'], 'label_id': 1}, data))
+    mapped_data = list(map(lambda x: {'timestamp': x['pub_date'], 'title': x['headline']
+                                      ['print_headline'], 'text': x['snippet'], 'link': x['web_url'], 'label_id': label_id}, data))
     op.bulk_insert(
         event_table,
         mapped_data
     )
-
 
 
 def downgrade():
